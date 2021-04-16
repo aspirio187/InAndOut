e3cb7c121c3bcdc730b32bbfa379861dfeb13fae
Soultan Hatsijev <soultan_98@hotmail.com>
2021-04-16 22:41:06 +0200

    11. Add new columns to database

--- InAndOut/InAndOut/Models/Item.cs
+++ InAndOut/InAndOut/Models/Item.cs
@@ -9,5 +9,7 @@ namespace InAndOut.Models
     {
         public int Id { get; set; }
         public string Borrower { get; set; }
+        public string Lender { get; set; }
+        public string ItemName { get; set; }
     }
 }
